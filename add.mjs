3aeafe1cf0cec2a005ef570d7e8c63eed98d31f8
Daniel Bokor <daniel.bokor@cognizant.com>
2025-03-11 12:52:32 +0200

    Using winston for logging

--- add.mjs
+++ add.mjs
@@ -1,6 +1,14 @@
 import chalk from "chalk";
+import winston from "winston";
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
+});
 
 export function add(a, b) {
-  console.log(chalk.blue("adding " + a + " + " + b));
+  logger.log("info", chalk.blue("adding " + a + " + " + b));
   return a + b;
 }
